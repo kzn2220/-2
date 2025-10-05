488527485aec5db7190197ea58f255885a543b93
kzn2220 <kuzn.a2220@gmail.com>
2025-10-05 16:11:41 +0700

    редактирование oceanmanager, прошлый коммит ошибочный

--- Практическая 2/OceanManager.cs
+++ Практическая 2/OceanManager.cs
@@ -11,5 +11,39 @@ namespace Практическая_2
         public List<Sea> Seas {  get; set; } = new List<Sea>();
         public List<SeaAnimal> Animals { get; set; } = new List<SeaAnimal>();
         public List<Island> Islands { get; set; } = new List<Island>();
+
+        public Sea FindDeepestSea()
+        {
+            if (Seas.Count == 0) return null;
+            return Seas.OrderByDescending(s => s.Depth).First();
+        }
+
+        public Sea FindSaltiestSea()
+        {
+            if (Seas.Count == 0) return null;
+            return Seas.OrderByDescending(s => s.Salinity).First();
+        }
+
+        public SeaAnimal FindMostPopulousAnimal()
+        {
+            if (Animals.Count == 0) return null;
+            return Animals.OrderByDescending(a => a.Population).First();
+        }
+
+        public List<SeaAnimal> FindAnimalsInSea(string seaName)
+        {
+            return Animals.Where(a => a.SeaName == seaName).ToList();
+        }
+
+        public Island FindLargestIsland()
+        {
+            if (Islands.Count == 0) return null;
+            return Islands.OrderByDescending(i => i.Square).First();
+        }
+
+        public List<Island> FindIslandsInSea(string seaName)
+        {
+            return Islands.Where(i => i.SeaName == seaName).ToList();
+        }
     }
 }
