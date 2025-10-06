e57fb66e18ded41d3ed59e786566bd21ec132393
kzn2220 <kuzn.a2220@gmail.com>
2025-10-06 12:21:31 +0700

    добавление класса Ship

--- Практическая 2/OceanManager.cs
+++ Практическая 2/OceanManager.cs
@@ -11,12 +11,14 @@ namespace Практическая_2
         public List<Sea> Seas {  get; set; }
         public List<SeaAnimal> Animals { get; set; }
         public List<Island> Islands { get; set; }
+        public List<Ship> Ships { get; set; }
 
-        public OceanManager(List<Sea> seas, List<SeaAnimal> animals, List<Island> islands)
+        public OceanManager(List<Sea> seas, List<SeaAnimal> animals, List<Island> islands, List<Ship> ships)
         {
             Seas = seas;
             Animals = animals;
             Islands = islands;
+            Ships = ships;
         }
 
         public Sea FindDeepestSea()
@@ -37,6 +39,11 @@ namespace Практическая_2
         public Island FindLargestIsland()
         {
             return Islands.OrderByDescending(i => i.Square).FirstOrDefault();
+        }
+
+        public Ship FindOldestShip()
+        {
+            return Ships.OrderBy(s => s.YearBuilt).FirstOrDefault();
         }
     }
 }
---
+++ Практическая 2/Ship.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_2
+{
+    internal class Ship
+    {
+        public string Name { get; set; }
+        public string SeaName { get; set; }
+        public string Type { get; set; }
+        public int YearBuilt { get; set; }
+
+        public Ship(string name, string seaName, string type, int yearBuilt)
+        {
+            Name = name;
+            SeaName = seaName;
+            Type = type;
+            YearBuilt = yearBuilt;
+        }
+
+        public string GetInfo()
+        {
+            return $"{Name} ({Type}) в {SeaName}, год постройки: {YearBuilt}";
+        }
+    }
+}
